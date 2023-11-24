da5e5f97d54d4401f75de9a949080d362074d17f
Archie Smith <archie.smith.as@icloud.com>
2023-11-24 17:35:10 +0000

    edit game.html

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -43,6 +43,10 @@ module.exports = {
       template: Path.resolve(__dirname, '../src/bank.html'),
       filename: 'bank.html', // Use [name] to dynamically generate filenames.
     }),
+    new HtmlWebpackPlugin({
+      template: Path.resolve(__dirname, '../src/game.html'),
+      filename: 'game.html', // Use [name] to dynamically generate filenames.
+    }),
   ],
   resolve: {
     alias: {
