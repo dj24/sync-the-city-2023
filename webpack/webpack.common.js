f0df0d69f8bbf8fbf23169d63e146ee1807e9c93
Daniel Jackson <danieljackson97123@gmail.com>
2023-11-24 12:25:22 +0000

    add login path

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -6,6 +6,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: {
     app: Path.resolve(__dirname, '../src/scripts/index.js'),
+    login: Path.resolve(__dirname, '../src/scripts/login.js'),
   },
   output: {
     path: Path.join(__dirname, '../build'),
@@ -24,6 +25,13 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: Path.resolve(__dirname, '../src/index.html'),
+      filename: 'index.html', // Use [name] to dynamically generate filenames.
+      chunks: ['index'], // Specify the entry point chunk for this HTML file.
+    }),
+    new HtmlWebpackPlugin({
+      template: Path.resolve(__dirname, '../src/login.html'),
+      filename: 'login.html', // Use [name] to dynamically generate filenames.
+      chunks: ['login'], // Specify the entry point chunk for this HTML file.
     }),
   ],
   resolve: {
